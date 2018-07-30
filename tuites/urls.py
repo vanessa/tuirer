9e0119d9ebe2783f409a7ef83bd894f67eb443fe
vanessa <vanessa@vinta.com.br>
2018-07-30 03:02:28 -0300

    Corrige urls

--- tuites/urls.py
+++ tuites/urls.py
@@ -5,9 +5,9 @@ from tuites.views import ListTuiteView, PostTuiteView, SearchTuiteView, SingleTu
 app_name = 'tuites'
 
 urlpatterns = [
-    path('postar', PostTuiteView.as_view(), name='post'),
-    path('tuites', ListTuiteView.as_view(), name='list'),
-    path('buscar', SearchTuiteView.as_view(), name='search'),
+    path('postar/', PostTuiteView.as_view(), name='post'),
+    path('tuites/', ListTuiteView.as_view(), name='list'),
+    path('buscar/', SearchTuiteView.as_view(), name='search'),
     path('tuite/<int:pk>', SingleTuiteView.as_view(), name='tuite'),
 
     # Actions
