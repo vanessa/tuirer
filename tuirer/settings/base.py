4034f5808c8a72b62c50ddbb93e7ccc5ba01b04c
vanessa <vanessa@vinta.com.br>
2018-08-11 02:07:04 -0300

    Reorganiza variáveis

--- tuirer/settings/base.py
+++ tuirer/settings/base.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = base_dir_join('assets')
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = base_dir_join('mediafiles')
 MEDIA_URL = '/media/'
