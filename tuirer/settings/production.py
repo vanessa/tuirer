838e28230483586fd7ac62c3d496e0c1bd3e7c16
vanessa <vanessa@vinta.com.br>
2018-08-11 01:50:22 -0300

    Adiciona django-storages com Dropbox pra produção

--- tuirer/settings/production.py
+++ tuirer/settings/production.py
@@ -2,6 +2,14 @@ from .base import *
 from decouple import config
 import django_heroku
 
+INSTALLED_APPS += ('django_storages', )
+DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
+
+# Lembre-se de adicionar essas variáveis
+# em "Environment variables" do Heroku!
+DROPBOX_OAUTH2_TOKEN = config('DROPBOX_OAUTH2_TOKEN')
+DROPBOX_ROOT_PATH = config('DROPBOX_ROOT_PATH')
+
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = config('SENDGRID_USERNAME')
 EMAIL_HOST_PASSWORD = config('SENDGRID_PASSWORD')
