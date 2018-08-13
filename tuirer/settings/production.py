dadde678d1245806cb53f990d050bcf94ce2c0b6
vanessa <vanessa@vinta.com.br>
2018-08-12 22:26:32 -0300

    Remove django_heroku.settings

--- tuirer/settings/production.py
+++ tuirer/settings/production.py
@@ -1,18 +1,19 @@
 from .base import *
 from decouple import config
 import django_heroku
+import dj_database_url
 
 # Lembre-se de adicionar essas variáveis
 # em "Environment variables" do Heroku!
 DROPBOX_OAUTH2_TOKEN = config('DROPBOX_OAUTH2_TOKEN')
 DROPBOX_ROOT_PATH = config('DROPBOX_ROOT_PATH')
 
+DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600)
+
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = config('SENDGRID_USERNAME')
 EMAIL_HOST_PASSWORD = config('SENDGRID_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-django_heroku.settings(locals(), staticfiles=False)
 
 DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
